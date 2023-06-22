08d701d05d66edcf8d0c6ebcd30e6f2bcfd1edcc
Joao Henrique Bitencourt <jh.bitencourt23@gmail.com>
2023-06-21 23:25:55 -0300

    Inicio

---
+++ Fundamentos/Inicio/Comentario.js
@@ -0,0 +1,11 @@
+// console.log é utilizado para exibir algo na tela do computador!
+console.log("Passo #01");
+console.log("Passo #02");
+/*
+   * Esse é um 
+   * comentario 
+   * de multiplas
+   * linhas.
+   * Varios asteriscos e utilizado apenas na estetica não e necessacio, somente no inicio e final   
+*/
+console.log("Passo #03");
