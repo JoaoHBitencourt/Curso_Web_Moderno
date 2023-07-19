a3297bc57cff8c705b48b708f8d709397e5b96a7
Joao Henrique Bitencourt <jh.bitencourt23@gmail.com>
2023-07-18 23:55:14 -0300

    Exercicios Bootstrap e projeto galeria

---
+++ Exercicios_Web/bootstrap/projeto-galeria/src/index.js
@@ -0,0 +1,10 @@
+// Arquivos SASS
+import './scss/index.scss'
+
+// Dependencias
+import 'jquery'
+import 'bootstrap'
+
+// Medus arquivos JS
+import './js/core/includes'
+import './js/plugins/cityButtons'
